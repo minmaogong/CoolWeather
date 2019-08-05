2eb5a8f7629e9ba614a30fe32e7fded4bcae84d0
gmm <gongmaomin@qq.com>
2019-08-05 13:39:30 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/gmm/coolweather/db/City.java
@@ -0,0 +1,44 @@
+package com.gmm.coolweather.db;
+
+/**
+ * 市
+ */
+public class City {
+
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceID;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceID() {
+        return provinceID;
+    }
+
+    public void setProvinceID(int provinceID) {
+        this.provinceID = provinceID;
+    }
+}
